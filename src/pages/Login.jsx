34ac183ac197e0a5f23181236bd51b00b3435cc7
syskraken <tripolefranklin67@gmail.com>
2026-09-17 22:08:13 +0800

    Minimal text changes

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -39,7 +39,7 @@ export default function Login() {
   return (
     <AuthLayout
       icon={LogIn}
-      title="Welcome back"
+      title="Seaview"
       subtitle="Log in to your account"
       footer={
         <>
